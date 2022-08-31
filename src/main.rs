c83613780076749d870b723c04a3c190de766c88
Squee Mueller <squeemishly@gmail.com>
2022-08-31 12:04:27 -0600

    implement syncing check

--- src/main.rs
+++ src/main.rs
@@ -5,44 +5,39 @@ use std::io::{Read, Write};
 #[derive(Debug)]
 pub struct DurableFile {
     file: File,
-    // needs_sync: bool,
+    needs_sync: bool,
 }
 
 impl  DurableFile {
     pub fn new(file: File) -> DurableFile {
-        DurableFile { file: file }
+        DurableFile { file: file, needs_sync: false }
     }
 }
 
 impl  Write for DurableFile {
     fn flush(&mut self) -> Result<(), std::io::Error> {
         self.file.sync_all()?;
+        self.needs_sync = false;
         self.file.flush()
     }
 
     fn write(&mut self, buf: &[u8]) -> Result<usize, std::io::Error> {
-        let stuff = self.file.write(buf)?;
-        dbg!("write");
-        dbg!(&stuff);
-
-        dbg!("test");
-        dbg!(&self.file);
-
-        let mut read_buf = [0; 5];
-        let stuff2 = self.file.read(&mut read_buf);
-        dbg!("read");
-        dbg!(&self.file);
-        dbg!(&stuff2);
-        Ok(stuff)
+        self.needs_sync = true;
+        self.file.write(buf)
     }
 }
 
 impl   Read for DurableFile   {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
-        let stuff = self.file.read(buf)?;
-        // dbg!("read");
-        // dbg!(&stuff);
-        Ok(stuff)
+        self.file.read(buf)
+    }
+}
+
+impl Drop for DurableFile {
+    fn drop(&mut self) {
+        if self.needs_sync {
+            panic!("file needs sync")
+        }
     }
 }
 
@@ -61,29 +56,29 @@ mod tests {
     #[test]
     fn write_test() {
         let dir = tempdir::TempDir::new("tests").unwrap();
-        {
-            let file = OpenOptions::new()
-                .read(true)
-                .write(true)
-                .create(true)
-                .open(dir.path().join("bar.txt"))
-                .unwrap();
-            let mut durable_file = DurableFile::new(file);
-            match durable_file.write(b"hello") {
-                Ok(_) => println!(""),
-                Err(d) => println!("didn't write file: {d}"),
-            };
-        }
-
         let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(dir.path().join("bar.txt"))
+            .unwrap();
+        let mut durable_file = DurableFile::new(file);
+        match durable_file.write(b"hello") {
+            Ok(_) => println!(""),
+            Err(d) => println!("didn't write file: {d}"),
+        };
+        let _ = durable_file.flush();
+
+        // rust has a full tantrum about reusing file descriptors
+        // to get around this, we just refile'd our file. 
+        let refile = OpenOptions::new()
             .read(true)
             .open(dir.path().join("bar.txt"))
             .unwrap();
-
-        let mut durable_file = DurableFile::new(file);
+        let mut durable_file2 = DurableFile::new(refile);
 
         let mut container_string = [0; 5];
-        let read_size = durable_file.read(&mut container_string).unwrap();
+        let read_size = durable_file2.read(&mut container_string).unwrap();
 
         assert_eq!(b"hello", &container_string);
         assert_eq!(5, read_size);
@@ -103,5 +98,22 @@ mod tests {
 
         assert_eq!(b"hello", &container_string);
         assert_eq!(5, read_size)
+    }
+
+    #[test]
+    #[should_panic(expected = "file needs sync")]
+    fn sync_fail_test() {
+        let dir = tempdir::TempDir::new("tests").unwrap();
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(dir.path().join("bar.txt"))
+            .unwrap();
+        let mut durable_file = DurableFile::new(file);
+        match durable_file.write(b"hello") {
+            Ok(_) => println!(""),
+            Err(d) => println!("didn't write file: {d}"),
+        };
     }
 }
